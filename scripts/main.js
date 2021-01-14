9bca4085dc899b668d62605650cb1aa68d8bef02
Mendel Groner <mendlegr@gmail.com>
2021-01-14 17:15:15 -0500

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/wicked.jpg') {
+        myImage.setAttribute('src', 'images/prep.jpg');
+    } else {
+        myImage.setAttribute('src', 'images/wicked.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+	let myName = prompt('Please enter your name:');
+	if (!myName) {
+		setUerName();
+		} else {
+		localStorage.setItem('name', myName);
+		myHeading.textContent = 'shainabank.com\nwelcomes you,\n' + myName + 'S!';
+		}
+	}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+	} else {
+	let storedName = localStorage.getItem('name');
+	myHeading.textContent = 'shainabank.com\nwelcomes you back,\n' + storedName + '!';
+	}
+
+myButton.onclick = function() { 
+	setUserName(); 
+	}
